e443922b65afb826270dd25975621acdb0968ea5
Burak ŞEN <buraksenb@gmail.com>
2021-12-25 11:39:28 +0300

    Implemented not-found error.

---
+++ src/errors/not-found.js
@@ -0,0 +1,9 @@
+const ApiError = require("./api-error");
+
+
+class NotFoundError extends ApiError {
+    constructor() {
+        super("Requested resource has not been found",404);
+    }
+}
+module.exports = NotFoundError
