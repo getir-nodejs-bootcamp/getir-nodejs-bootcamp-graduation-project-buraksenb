6720977ec13fd70980dd16e59e8c7dcd1c11229e
Burak ŞEN <buraksenb@gmail.com>
2021-12-26 13:39:11 +0300

    Changed db function for logging.

--- src/loaders/index.js
+++ src/loaders/index.js
@@ -1,6 +1,5 @@
-const { connectDB } = require("./mongodb");
+const {connectDB} = require("./mongodb");
 
 module.exports = () => {
-    connectDB().then(() => console.log("Connection to MongoDB is successful."))
-        .catch(() => console.log("Connection to MongoDB is NOT successful."));
+    connectDB()
 };
--- src/loaders/mongodb.js
+++ src/loaders/mongodb.js
@@ -1,10 +1,16 @@
-const Mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-const connectDB = async (uri, callback) => {
-    await Mongoose.connect(process.env.DB_CONNECTION_STRING, {
+
+const connectDB = (uri, callback) => {
+    mongoose.connect(process.env.DB_CONNECTION_STRING, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-    });
+    }).catch(e => console.log(e?.message))
+    mongoose.connection.on('connected', () => console.log("Connection to MongoDB is successful"))
+    mongoose.connection.on('disconnected', () => console.log("Disconnected from MongoDB"))
+    mongoose.connection.on('error', e => console.log(e?.message));
+
+
 };
 
-module.exports = { connectDB };
+module.exports = {connectDB};
